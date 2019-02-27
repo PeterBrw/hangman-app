3efa3579d8f6d49f8c4e9661b4bcf01dc55932f7
Petru <peter.giurgiu@gmail.com>
2019-02-28 01:42:57 +0200

    96-hangman-challenge:-part-ii

--- hangman.js
+++ hangman.js
@@ -1,13 +1,13 @@
-const Hangman = function(word, remainingGuesses, guesses) {
+const Hangman = function(word, remainingGuesses) {
   this.word = word.toLowerCase().split('')
   this.remainingGuesses = remainingGuesses
-  this.guesses = ['c', 'e']
+  this.guessedLetters = []
 }
 
 Hangman.prototype.getPuzzle = function() {
   let result = ''
   this.word.forEach((letter) => {
-    if(this.guesses.includes(letter)) {
+    if(this.guessedLetters.includes(letter)) {
       result += letter
     } else if(letter === ' ') {
       result += ' '
@@ -19,8 +19,29 @@ Hangman.prototype.getPuzzle = function() {
   return result
 }
 
-const game1 = new Hangman('Cat', 2)
-console.log(game1.getPuzzle())
+Hangman.prototype.makeGuess = function(guess) {
+  guess = guess.toLowerCase()
+  const isUnique = !this.guessedLetters.includes(guess)
+  const isBadGuess = !this.word.includes(guess)
 
-const game2 = new Hangman('New Jersey', 4)
-console.log(game2.getPuzzle())
+  if(isUnique) {
+    this.guessedLetters.push(guess)
+  }
+
+  if(isUnique && isBadGuess) {
+    this.remainingGuesses--
+  }
+}
+
+const game1 = new Hangman('Cat', 2)
+
+console.log(game1.getPuzzle())
+console.log(game1.remainingGuesses)
+
+window.addEventListener('keypress', function(e) {
+  const guess = String.fromCharCode(e.charCode)
+  game1.makeGuess(guess)
+  console.log(game1.getPuzzle())  
+  console.log(game1.remainingGuesses)
+
+})
