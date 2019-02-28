b5b69c9a8da3964ca00a4806e8d2d751bdf47c87
Petru <peter.giurgiu@gmail.com>
2019-02-28 18:14:38 +0200

    98-hangman-challenge-part-iv

--- hangman.js
+++ hangman.js
@@ -30,9 +30,17 @@ Hangman.prototype.calculateStatus = function() {
   } else {
     this.status = 'playing'
   }
-
 }
 
+Hangman.prototype.getStatusMessage = function() {
+  if(this.status === 'playing') {
+    return `Guesses left: ${this.remainingGuesses}`
+  } else if(this.status === 'failed') {
+    return `Nice try! The word was "${this.word.join('')}".`
+  } else {
+    return 'Great work! You guessed the work.'
+  }
+}
 
 Hangman.prototype.getPuzzle = function() {
   let result = ''
@@ -53,6 +61,10 @@ Hangman.prototype.makeGuess = function(guess) {
   guess = guess.toLowerCase()
   const isUnique = !this.guessedLetters.includes(guess)
   const isBadGuess = !this.word.includes(guess)
+
+  if(this.status !== 'playing') {
+    return 
+  }
 
   if(isUnique) {
     this.guessedLetters.push(guess)
