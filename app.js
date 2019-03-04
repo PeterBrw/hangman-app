39711e86fdc80d19d62fc14e8b4ce03637b73166
Petru <peter.giurgiu@gmail.com>
2019-03-05 00:55:51 +0200

    107-callback-abstraction

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const game1 = new Hangman('Car Parts', 2)
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
 
-
 window.addEventListener('keypress', (e) => {
   const guess = String.fromCharCode(e.charCode)
   game1.makeGuess(guess)
@@ -13,33 +12,30 @@ window.addEventListener('keypress', (e) => {
   guessesEl.textContent = game1.statusMessage
 })
 
-// Making an HTTP request
-// const request = new XMLHttpRequest()
-
-// request.addEventListener('readystatechange', (e) => {
-//   if(e.target.readyState === 4 && e.target.status === 200 ){
-//     const data = JSON.parse(e.target.responseText)
-//     console.log(data)
-//   } else if(e.target.readyState === 4) {
-//     console.log('An error has taken place')
-//   }
-// })
-
-// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-// request.send()
-
-const countryCode = "US"
-const countryRequest = new XMLHttpRequest()
-
-countryRequest.addEventListener('readystatechange', (e) => {
-  if(e.target.readyState === 4 && e.target.status === 200) {
-    const countries = JSON.parse(e.target.responseText)
-    const country = countries.find((c) => c.alpha2Code === countryCode)
-    console.log(country.name)
-  } else if(e.target.readyState === 4) {
-    console.log('Unable to fetch data')
+getPuzzle((error, puzzle) => {
+  if(error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(puzzle)
   }
 })
 
-countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
-countryRequest.send()
+
+// Making an HTTP request
+
+
+// const countryCode = "US"
+// const countryRequest = new XMLHttpRequest()
+
+// countryRequest.addEventListener('readystatechange', (e) => {
+//   if(e.target.readyState === 4 && e.target.status === 200) {
+//     const countries = JSON.parse(e.target.responseText)
+//     const country = countries.find((c) => c.alpha2Code === countryCode)
+//     console.log(country.name)
+//   } else if(e.target.readyState === 4) {
+//     console.log('Unable to fetch data')
+//   }
+// })
+
+// countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
+// countryRequest.send()
