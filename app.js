7795a9690d902cda892217f8bc8a0208626da083
Petru <peter.giurgiu@gmail.com>
2019-03-06 18:46:37 +0200

    114-the-fetch-api

--- app.js
+++ app.js
@@ -12,21 +12,36 @@ window.addEventListener('keypress', (e) => {
   guessesEl.textContent = game1.statusMessage
 })
 
-getPuzzle('1')
-  .then(data => {
-    console.log(data)
+getPuzzle('2')
+  .then(puzzle => {
+    console.log(puzzle)
   })
   .catch(err => {
     console.log(err)
   })
 
-// Making an HTTP request
+// // Making an HTTP request
 
 
-getCountry('RO')
-  .then(response => {
-    console.log(response)
-  })  
-  .catch(err => {
-    console.log(err)
-  })
+// getCountry('RO')
+//   .then(response => {
+//     console.log(response)
+//   })  
+//   .catch(err => {
+//     console.log(err)
+//   })
+
+// fetch('http://puzzle.mead.io/puzzle', {})
+//   .then((response) => {
+//     if(response.status === 200) {
+//       return response.json()
+//     } else {  
+//       throw new Error('Unable to fetch the puzzle')
+//     }
+//   })
+//   .then((data) => {
+//     console.log(data.puzzle)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
