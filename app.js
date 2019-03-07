6b7ca609ee62e09d58427a524439ea5e21606ae4
Petru <peter.giurgiu@gmail.com>
2019-03-07 10:47:42 +0200

    119-integrating-data-into-the-application

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('Car Parts', 2)
-
-puzzleEl.textContent = game1.puzzle
-guessesEl.textContent = game1.statusMessage
+let game1 
 
 window.addEventListener('keypress', (e) => {
   const guess = String.fromCharCode(e.charCode)
   game1.makeGuess(guess)
-  puzzleEl.textContent = game1.puzzle
-  guessesEl.textContent = game1.statusMessage
+  render()
 })
 
-getPuzzle('2')
-  .then(puzzle => {
-    console.log(puzzle)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+const render = () => {
+  puzzleEl.textContent = game1.puzzle
+  guessesEl.textContent = game1.statusMessage
+}
+
+const startGame = async () => {
+  const puzzle = await getPuzzle('2')
+  game1 = new Hangman(puzzle, 5)
+  render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
+
+// getPuzzle('2')
+//   .then(puzzle => {
+//     console.log(puzzle)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
 
 // // Making an HTTP request
 
 
-getCountry('RO')
-  .then(country => {
-    console.log(country)
-  })
-  .catch(error => {
-    console.log(error)
-  })
+// getCountry('RO')
+//   .then(country => {
+//     console.log(country)
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
 
 // getCountry('RO')
 //   .then(response => {
